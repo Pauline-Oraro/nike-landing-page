3bc2a9247c51442ff27346cc40bb6031078bf386
Pauline-Oraro <hiltrapauline@gmail.com>
2025-06-29 16:41:51 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div className='bg-violet-400 font-mono p-5 m-3 border-2 border-amber-800'>nike landing page</div>
+  )
+}
+
+export default App
