c49b3285f3ac474fa6685d0c892cfbc016164c91
Pauline-Oraro <hiltrapauline@gmail.com>
2025-07-01 14:20:16 +0300

    Navigation

---
+++ src/components/Nav.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { navLinks } from '../constants';
+
+const Nav = () => {
+  return (
+    <header className='padding-x py-8 absolute z-10 w-full'>
+      <nav className='flex justify-between items-center max-container'>
+        <a href="/">
+        <img src="/src/assets/images/header-logo.svg" 
+        alt="logo" 
+        className='m-0 w-[129px] h-[29px]' />
+        </a>
+        {/*visible on desktop devices */}
+        <ul className='flex-1/2 flex justify-center items-center gap-16 max-lg:hidden'>
+        {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className='font-montserrat leading-normal text-lg text-slate-gray'
+              >
+                {item.label}
+              </a>
+            </li>
+        ))}
+        </ul>
+        {/*hamburger menu on mobile devices */}
+        <div className='hidden max-lg:block'>
+            <img src="/src/assets/icons/hamburger.svg" alt="hamburger menu" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Nav
